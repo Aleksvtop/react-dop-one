bed992ad7770ca58c0b61f6569c2fac09db0f7dd
Aleksvtop <vazilino@gmail.com>
2023-01-17 12:00:46 +0300

    Create project for Heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type PropsType={
+    body: string
+    id: number
+    title: string
+    userId: number
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        const [state, setState] = useState<PropsType[]>( [])
+
+        useEffect(() => {
+            fetch('https://jsonplaceholder.typicode.com/posts').
+            then(response => response.json()).
+            then(json => setState(json))
+        }, [])
+        const clearAll = () => {
+            setState([])
+        }
+        const getData = () => {
+            fetch('https://jsonplaceholder.typicode.com/posts').
+            then(response => response.json()).
+            then(json => setState(json))
+        }
+    return (
+        <div className="App">
+            <button onClick={clearAll}>Clear all</button>
+            <button onClick={getData}>Get data</button>
+            <ul>
+                {state.map((el) => {
+                    return (
+                        <li key={el.id}>
+                            <span>{el.id}</span>
+                            <span>{el.userId}</span>
+                            <span>{el.title}</span>
+                        </li>
+                    )})
+                }
+            </ul>
+
+
+        </div>
+    );
 }
 
 export default App;
